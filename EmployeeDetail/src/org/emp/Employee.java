d8a1781742833b37860312f0b5685e4301c2c316
SHemamalini <146621405+SHemamalini@users.noreply.github.com>
2024-09-23 21:26:40 -0700

    Add files via upload

---
+++ EmployeeDetail/src/org/emp/Employee.java
@@ -0,0 +1,35 @@
+package org.emp;
+
+public class Employee {
+	private void empId() {
+        System.out.println("E21IT");
+	}
+	private void empName() {
+		System.out.println("Hema");
+	}
+	private void empDob() {
+        System.out.println("3/05/2004");
+	}
+	private void empPhone() {
+        System.out.println("346729633");
+	}
+	private void empEmail() {
+		System.out.println("ghesiwnrig@gmail.com");
+
+	}
+	private void empAddress() {
+       System.out.println("ABC");
+	}
+	public static void main(String[] args) {
+		Employee a=new Employee();
+			a.empId();
+			a.empName();
+			a.empDob();
+			a.empPhone();
+			a.empEmail();
+			a.empAddress();
+			
+		
+	}
+
+}
