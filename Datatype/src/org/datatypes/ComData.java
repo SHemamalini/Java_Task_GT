ab0001924245a23298d9214f222c1bcea5477b47
SHemamalini <146621405+SHemamalini@users.noreply.github.com>
2024-09-23 21:06:29 -0700

    Add files via upload

---
+++ Datatype/src/org/datatypes/ComData.java
@@ -0,0 +1,28 @@
+package org.datatypes;
+
+public class ComData {
+//datatype===>variable name=values  Syntax
+	public static void main(String[] args) {
+		String name="Hema";
+		//System.out.println("emp name is"+name);
+		byte age=23;
+		//System.out.println("emp age is"+age);
+		long mobile=1234567890;
+		//System.out.println("emp mobile is"+mobile);
+		char gender='F';
+		//System.out.println("emp gender is"+gender);
+		short salary=30000;
+		//System.out.println("emp salary is"+salary);
+		int accno=3759275;
+		//System.out.println("emp accno is"+accno);
+		float p=2.3f;
+		//System.out.println("emp float is"+p);
+		double d=23.5870384;
+		//System.out.println("emp double is"+d);
+		boolean marital=true;
+		System.out.println("emp marital is "+marital +"\n" +"emp name is "+name +"\n"+"emp age is "+age+"\n" 
+		+"emp mobile is "+mobile +"\n" +"emp gender is "+gender );
+		
+				
+	}
+}
