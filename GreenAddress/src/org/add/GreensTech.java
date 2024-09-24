f018b7bfd9ad15de37ce6060898887c7fc32621e
SHemamalini <146621405+SHemamalini@users.noreply.github.com>
2024-09-23 21:25:18 -0700

    Add files via upload

---
+++ GreenAddress/src/org/add/GreensTech.java
@@ -0,0 +1,31 @@
+package org.add;
+
+public class GreensTech {
+	private void greensOmr() {
+       System.out.println();
+	}
+	private void greensAdayar() {
+		System.out.println("Omr");
+
+	}
+	private void greensTambaram() {
+		System.out.println("Tambaram");
+
+	}
+	private void greensVelacherry() {
+		System.out.println("Velacherry");
+	}
+	private void greensAnnaNagar() {
+		System.out.println("AnnaNagar");
+	}
+	public static void main(String[] args) {
+		GreensTech s=new GreensTech();
+		s.greensOmr();
+		s.greensAdayar();
+		s.greensTambaram();
+	    s.greensVelacherry();
+	    s.greensAnnaNagar();
+		
+	}
+
+}
